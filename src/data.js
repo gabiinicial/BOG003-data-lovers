157ad88bcd8a9fc46e12fc49f3723b66cc142632
gabriela <gabipaez572018@gmail.com>
2021-07-22 08:59:05 -0500

    imprimio la data y comenzando a hacer los metodos

--- src/data.js
+++ src/data.js
@@ -1,9 +1,35 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
 
-export const anotherExample = () => {
-  return 'OMG';
+function filterData(data, condition) {
+
+  let pokemonsFilter = [];
+
+  for (let i = 0; i < data.pokemon.length; i++) {
+
+    for (let types = 0; data.pokemon[i].type; types++) {
+      if (data.pokemon[i].type[types] === condition) {
+        pokemonsFilter.push(data.pokemon[i]);
+      }
+    }
+  }
+
+  return pokemonsFilter;
+}
+
+function sortData(data, sortBy, sortOrder) {
+
+  let pokemonsOrder = [];
+  for (let i = 0; i < data.pokemon.length; i++) {
+
+    /*for (let names = 0; data.pokemon[i].name; i++) {
+      if (data.pokemon.[].names === sortBy);
+      pokemonsOrder.push(data.pokemon[i]);*/
+
+    }
+  }
+
+
+export {
+  filterData,
+  sortData
 };
--- src/main.js
+++ src/main.js
@@ -1,25 +1,52 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import {
+  filterData,
+  sortData
+} from './data.js';
+
 import data from './data/pokemon/pokemon.js';
+// import data from './data/lol/lol.js';
+
 // import data from './data/rickandmorty/rickandmorty.js';
 //Boton descarga la APP
+
 document.getElementById("btngo").addEventListener("click", () => {
-    document.getElementById("page1").style.display='block';
+  document.getElementById("page1").style.display = 'block';
+
 })
 document.getElementById("btnapp").addEventListener("click", () => {
-    location.href = "https://pokemongolive.com/es/";
+  location.href = "https://pokemongolive.com/es/";
 })
 document.getElementById("btnj").addEventListener("click", () => {
-    location.href = "https://pokemongolive.com/es/play-where-you-are/";
+  location.href = "https://pokemongolive.com/es/play-where-you-are/";
 })
 //Boton de Pokebola/
 document.getElementById("poke").addEventListener("click", () => {
-    document.getElementById('page1').style.display = 'none';
-    document.getElementById('page2').removeAttribute('hidden');
-    document.getElementById('page2').style.display = 'block';
-  });
+  document.getElementById('page1').style.display = 'none';
+  document.getElementById('page2').removeAttribute('hidden');
+  document.getElementById('page2').style.display = 'block';
+  document.getElementById('card').removeAttribute('hidden');
+});
+//recorrido para traer la data y agregarla
 
+function allPokemon(){
+  
+    for (let i = 0; i < data.pokemon.length; i++) {
 
+      const imag = document.createElement("img");
+      imag.src = data.pokemon[i].img
+      document.getElementById("card").appendChild(imag);
+    
+      const nameT = document.createElement("p");
+      nameT.innerText = data.pokemon[i].name + data.pokemon[i].num;
+      document.getElementById("card").appendChild(nameT);
+    }  
+}
 
-
-console.log(example, data);
+// funcion que se trae todos los pokemon
+allPokemon();
+/*
+    const nameT = document.createElement("p");
+    nameT.innerText = data.pokemon[i].name + data.pokemon[i].num;
+    document.getElementById("information").appendChild(nameT);
+  }*/
+//console.log(example , data);
