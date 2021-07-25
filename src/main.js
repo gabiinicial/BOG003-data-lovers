8123bd32c8895a2cc3bc92f867755bf86668d38c
gabriela <gabipaez572018@gmail.com>
2021-07-24 20:07:58 -0500

    ajuste del bucle for a forEach y dibujar pokemones al inicio

--- src/main.js
+++ src/main.js
@@ -25,81 +25,61 @@ document.getElementById("poke").addEventListener("click", () => {
   document.getElementById('page2').removeAttribute('hidden');
   document.getElementById('page2').style.display = 'block';
   document.getElementById('card').removeAttribute('hidden');
+  callPoke(datPoke);
 });
 // funcion que se trae todos los pokemon
-let datpoke = data.pokemon;
-function allPokemon() {
+let datPoke = data.pokemon;
+const callPoke = (datPoke) => {   
+  document.getElementById("card").textContent = "";
+ 
   //recorrido para traer la data y agregarla
-  for (let i = 0; i < data.pokemon.length; i++) {
+  datPoke.forEach((e) => {
 
-    const imag = document.createElement("img");
-    imag.src = data.pokemon[i].img
-    document.getElementById("card").appendChild(imag);
+      const imag = document.createElement("img")
+      imag.src = e.img;
+      document.getElementById("card").appendChild(imag);
 
-    const nameT = document.createElement("p");
-    nameT.innerText = data.pokemon[i].name + data.pokemon[i].num;
-    document.getElementById("card").appendChild(nameT);
+      const nameT = document.createElement("p");
+      nameT.innerText = e.name + e.num;
+      document.getElementById("card").appendChild(nameT);
 
-    const types = document.createElement("p");
-    types.innerText = data.pokemon[i].type;
-    document.getElementById("card").appendChild(types);
+      const types = document.createElement("p");
+      types.innerText = e.type;
+      document.getElementById("card").appendChild(types);
 
-    const weaknessesP = document.createElement("p");
-    weaknessesP.innerText = data.pokemon[i].weaknesses;
-    document.getElementById("card").appendChild(weaknessesP);
+      const weaknessesP = document.createElement("p");
+      weaknessesP.innerText = e.weaknesses;
+      document.getElementById("card").appendChild(weaknessesP);
+    });
   }
-}
-//allPokemon();
+ 
 
-function drawPokemon(cardsPokemons) {
+  ///console.log (data.pokemon["type"])
   //recorrido para traer la data y agregarla
-  for (let i = 0; i < cardsPokemons.length; i++) {
+  //funciones para los selectores
 
-    const imag = document.createElement("img");
-    imag.src = cardsPokemons[i].img
-    document.getElementById("card").appendChild(imag);
+  /*let election = document.getElementById('type');
+  let typePoke = document.getElementById('card');
+  election.addEventListener('click', valueSelect);
+  function valueSelect() {
+    let valueElect = election.value;
+    if (valueElect == 'fire')
+    console.log (filterData, typePoke)
+    
+    /*parrafo = filterData.allPokemon;
+    }
 
-    const nameT = document.createElement("p");
-    nameT.innerText = cardsPokemons[i].name + cardsPokemons[i].num;
-    document.getElementById("card").appendChild(nameT);
+  }*/
 
-    const types = document.createElement("p");
-    types.innerText = cardsPokemons[i].type;
-    document.getElementById("card").appendChild(types);
-
-    const weaknessesP = document.createElement("p");
-    weaknessesP.innerText = cardsPokemons[i].weaknesses;
-    document.getElementById("card").appendChild(weaknessesP);
-  }
-}
-//allPokemon();
-
-///console.log (data.pokemon["type"])
-//recorrido para traer la data y agregarla
-//funciones para los selectores
-
-/*let election = document.getElementById('type');
-let typePoke = document.getElementById('card');
-election.addEventListener('click', valueSelect);
-function valueSelect() {
-  let valueElect = election.value;
-  if (valueElect == 'fire')
-  console.log (filterData, typePoke)
-  
-  /*parrafo = filterData.allPokemon;
-  }
-
-}*/
-
-document.getElementById("type").addEventListener("change", () => {
-  //valueSelect();//
-  const type = document.getElementById("type").value;
-  const colum = document.getElementById("colum").value;
-  const alfabet = document.getElementById("alfabet").value;
-  //alert('type: ' + type + ' colum: ' +  colum + ' alfabet: ' + alfabet);
-  const filterPoke = filterData(data, type);
-  const orderPoke = sortData (filterPoke,colum,alfabet);
-  drawPokemon (orderPoke);
+  document.getElementById("type").addEventListener("change", () => {
+    //valueSelect();//
+    const type = document.getElementById("type").value;
+    const colum = document.getElementById("colum").value;
+    const alfabet = document.getElementById("alfabet").value;
+    //alert('type: ' + type + ' colum: ' +  colum + ' alfabet: ' + alfabet);
+    const filterPoke = filterData(data, type);
+    const orderPoke = sortData(filterPoke, colum, alfabet);
+    callPoke(orderPoke);
   });
 
   document.getElementById("colum").addEventListener("change", () => {
@@ -108,30 +88,31 @@ document.getElementById("type").addEventListener("change", () => {
     const colum = document.getElementById("colum").value;
     const alfabet = document.getElementById("alfabet").value;
     //alert('type: ' + type + ' colum: ' +  colum + ' alfabet: ' + alfabet);
-    const filterPoke = filterData(data, colum);
-    const orderPoke = sortData (filterPoke,type,alfabet);
-    drawPokemon (orderPoke);
-    });
-    document.getElementById("alfabet").addEventListener("change", () => {
-      //valueSelect();//
-      const type = document.getElementById("type").value;
-      const colum = document.getElementById("colum").value;
-      const alfabet = document.getElementById("alfabet").value;
-      //alert('type: ' + type + ' colum: ' +  colum + ' alfabet: ' + alfabet);
-      const filterPoke = filterData(data, alfabet);
-      const orderPoke = sortData (filterPoke,type,colum);
-      drawPokemon (orderPoke);
-      });
+    const filterPoke = filterData(data, type);
+    const orderPoke = sortData(filterPoke, alfabet);
+    callPoke(orderPoke);
+    
+  });
+  document.getElementById("alfabet").addEventListener("change", () => {
+    //valueSelect();//
+    const type = document.getElementById("type").value;
+    const colum = document.getElementById("colum").value;
+    const alfabet = document.getElementById("alfabet").value;
+    //alert('type: ' + type + ' colum: ' +  colum + ' alfabet: ' + alfabet);
+    const filterPoke = filterData(data, type);
+    const orderPoke = sortData(filterPoke, colum);
+    callPoke(orderPoke);
+  });
 
 
-  
-/*const election = document.getElementById("type");
-function valueSelect() {
-  let valueElect = election.value;
-  if ( valueElect == "ice") {
-    print();
-}
-}
-function print(){
- document.getElementById. innerHTML = filterData;
-}*/
+
+  /*const election = document.getElementById("type");
+  function valueSelect() {
+    let valueElect = election.value;
+    if ( valueElect == "ice") {
+      print();
+  }
+  }
+  function print(){
+   document.getElementById. innerHTML = filterData;
+  }*/
